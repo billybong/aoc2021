c9d72863ce29e1246d683c7e57f57b5a185d3958
Billy Sjöberg <billy.sjoberg@gmail.com>
2021-11-29 14:00:29 +0100

    stream lines

--- day1/App.java
+++ day1/App.java
@@ -4,10 +4,11 @@ import java.nio.file.Paths;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        var numbers = Files.readAllLines(Paths.get("input.txt")).stream().mapToInt(Integer::valueOf);
-        var result = "1".equals(System.getenv("solution")) ?
-                numbers.sum() :
-                numbers.reduce(1, Math::multiplyExact);
-        System.out.println(result);
+        try (var numbers = Files.lines(Paths.get("input.txt")).mapToInt(Integer::valueOf)) {
+            var result = "1".equals(System.getenv("solution")) ?
+                    numbers.sum() :
+                    numbers.reduce(1, Math::multiplyExact);
+            System.out.println(result);
+        }
     }
 }
