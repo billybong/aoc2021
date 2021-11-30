9eeeaefae9dfd83c61bec90fdd30596a6f4e597e
Billy Sjöberg <billy.sjoberg@mojang.com>
2021-11-30 12:50:33 +0100

    use jdk int binary operator

--- day01/src/App.java
+++ day01/src/App.java
@@ -1,25 +1,23 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.function.IntBinaryOperator;
 import java.util.stream.IntStream;
 
 public class App {
 
     public static void main(String[] args) throws IOException {
-        final IntStream lines = Files.readAllLines(Paths.get("input.txt")).stream().mapToInt(Integer::parseInt);
-        final BiIntFunction mapper = "part1".equals(System.getenv("part")) ?
+        final IntBinaryOperator operator = "part1".equals(System.getenv("part")) ?
                 (index, number) -> isPrime(number) ? number * index : 0 :
                 (index, number) -> isPrime(number) ? 0 : index % 2 == 0 ? number : -number;
         final int[] index = {-1};
-        System.out.println(lines.reduce(0, (prev, current) -> prev + mapper.apply(++index[0], current)));
+        final int result = Files.readAllLines(Paths.get("input.txt")).stream()
+                .mapToInt(Integer::parseInt)
+                .reduce(0, (prev, current) -> prev + operator.applyAsInt(++index[0], current));
+        System.out.println(result);
     }
 
     private static boolean isPrime(int number) {
         return number > 1 && IntStream.rangeClosed(2, (int) Math.sqrt(number)).noneMatch(n -> (number % n == 0));
-    }
-
-    @FunctionalInterface
-    interface BiIntFunction {
-        int apply(int index, int number);
     }
 }
