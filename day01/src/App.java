d0c1b2d01b8f2ea9247bf6929e9916eb491301a4
Billy Sjöberg <billy.sjoberg@gmail.com>
2021-12-01 23:23:51 +0100

    Thanks Tobe for reminding me about math ;-)

--- day01/src/App.java
+++ day01/src/App.java
@@ -1,13 +1,12 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayDeque;
 import java.util.stream.IntStream;
 
 public class App {
     public static void main(String[] args) throws IOException {
         try (var numbers = Files.lines(Paths.get("input.txt")).mapToInt(Integer::parseInt)) {
-            System.out.println("part2".equals(System.getenv("part")) ? part2(numbers, 3) : part1(numbers));
+            System.out.println("part2".equals(System.getenv("part")) ? part2(numbers.toArray(), 3) : part1(numbers));
         }
     }
 
@@ -22,28 +21,14 @@ public class App {
         return count[0];
     }
 
-    private static long part2(IntStream numbers, int windowSize) {
-        var windows = new ArrayDeque<MutableInt>(windowSize + 1);
-
-        return numbers.peek(nr -> {
-                    windows.forEach(value -> value.increment(nr));
-                    windows.add(new MutableInt(nr));
-                })
-                .skip(windowSize)
-                .map(nr -> windows.removeFirst().value - nr)
-                .filter(head -> head < windows.peekFirst().value)
-                .count();
-    }
-
-    static class MutableInt {
-        int value;
-
-        private MutableInt(int initial) {
-            this.value = initial;
+    private static long part2(int[] numbers, int windowSize) {
+        int result = 0;
+        final int lastIndex = numbers.length - windowSize;
+        for (int i = 0; i < lastIndex; i++) {
+            if (numbers[i] < numbers[i + windowSize]) {
+                result++;
+            }
         }
-
-        public void increment(int delta) {
-            value += delta;
-        }
+        return result;
     }
 }
