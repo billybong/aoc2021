b768632b5765361920e7c18d6fd580d2f4478c41
Billy Sjöberg <billy.sjoberg@gmail.com>
2021-12-02 14:39:08 +0100

    blue collar day 1

--- day01/src/App.java
+++ day01/src/App.java
@@ -1,24 +1,23 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.stream.IntStream;
 
 public class App {
     public static void main(String[] args) throws IOException {
-        try (var numbers = Files.lines(Paths.get("input.txt")).mapToInt(Integer::parseInt)) {
-            System.out.println("part2".equals(System.getenv("part")) ? part2(numbers.toArray(), 3) : part1(numbers));
-        }
+        var numbers = Files.lines(Paths.get("input.txt")).mapToInt(Integer::parseInt).toArray();
+        System.out.println("part2".equals(System.getenv("part")) ? part2(numbers, 3) : part1(numbers));
     }
 
-    private static int part1(IntStream numbers) {
-        int[] count = new int[]{0};
-        numbers.reduce((prev, current) -> {
-            if (current > prev) {
-                count[0]++;
+    private static int part1(int[] numbers) {
+        int count = 0;
+        int prev = Integer.MAX_VALUE;
+        for (int number : numbers) {
+            if (number > prev) {
+                count++;
             }
-            return current;
-        });
-        return count[0];
+            prev = number;
+        }
+        return count;
     }
 
     private static int part2(int[] numbers, int windowSize) {
