a0a07659a0de7a1526cb37cd0bde4473d058b593
Billy Sjöberg <billy.sjoberg@mojang.com>
2021-12-07 09:03:40 +0100

    Kalle is brilliant

--- day06/src/App.java
+++ day06/src/App.java
@@ -7,27 +7,29 @@ import java.util.stream.LongStream;
 public class App {
     public static void main(String[] args) throws IOException {
         final int days = "part2".equals(System.getenv("part")) ? 256 : 80;
-        try (final BufferedReader br = new BufferedReader(new FileReader("input.txt"))){
+        try (final BufferedReader br = new BufferedReader(new FileReader("input.txt"))) {
             System.out.println(solution(br.readLine(), days));
         }
     }
+
     private static long solution(String line, int days) {
-        final long[] fishesByDaysUntilReproduction = new long[9];
+        final long[] ages = new long[9];
         final StringTokenizer tokenizer = new StringTokenizer(line, ",");
         while (tokenizer.hasMoreElements()) {
-            fishesByDaysUntilReproduction[Integer.parseInt(tokenizer.nextToken())]++;
+            ages[Integer.parseInt(tokenizer.nextToken())]++;
         }
 
-        long previousBucket = 0;
         for (int day = 0; day < days; day++) {
-            for (int i = fishesByDaysUntilReproduction.length - 1; i > 0; i--) {
-                final long tempBucket = fishesByDaysUntilReproduction[i];
-                fishesByDaysUntilReproduction[i] = previousBucket;
-                previousBucket = tempBucket;
-            }
-            fishesByDaysUntilReproduction[7] += previousBucket;
+            final int groupAtDay0 = day % 9;
+            // oh Java, why don't you have Python and Groovy's nth-to-last element accessors!?
+            final int groupForNewborns = switch (groupAtDay0) {
+                case 0 -> 7;
+                case 1 -> 8;
+                default -> groupAtDay0 - 2;
+            };
+            ages[groupForNewborns] += ages[groupAtDay0];
         }
 
-        return LongStream.of(fishesByDaysUntilReproduction).sum();
+        return LongStream.of(ages).sum();
     }
 }
